de86a132a0392ce4c555c6bb912de66a5eac9498
zMaSs4cR3x <106979384+zMassacrex@users.noreply.github.com>
2024-12-25 19:26:38 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -1,5 +1,5 @@
 {
-    "version": "1.2",
-    "changeLogs": "Version 1.2 - FIX ERROR No Such export AddMoney!",
-    "releaseDate": "10/30/24"
+    "version": "1.22.3",
+    "changeLogs": "Updates Available On!",
+    "releaseDate": "12/25/24"
 }
