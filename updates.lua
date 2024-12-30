8ec28d27a3fee53778c99c3e423eb0129def0e42
zMaSs4cR3x <106979384+zMassacrex@users.noreply.github.com>
2024-12-30 12:28:17 -0500

    Update updates.lua

--- updates.lua
+++ updates.lua
@@ -1,5 +1,5 @@
 {
-    "version": "1.0.0",
+    "version": "1.0.1",
     "changeLogs": "Updates Available.. Puedes descargarlos directo desde mi discord canal de qbox updates!",
     "releaseDate": "12/25/24"
 }
